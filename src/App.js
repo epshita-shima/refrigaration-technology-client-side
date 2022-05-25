ad74a9b328dcb83f413f5a0c74ceeeac91868a31
epshita-shima <epshitashima@gmail.com>
2022-05-25 13:56:57 +0600

    home add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
